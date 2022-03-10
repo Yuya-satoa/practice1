1aebbbb8bdb50087854f1927ed5894fc299f9450
Yuya Sato <yuuya.satou.sys21@morijyobi.ac.jp>
2022-03-10 10:21:50 +0900

    aaaaaaaa bbbbbbbb cccccccc

--- pra2.java
+++ pra2.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
-System.out.println("Hello World")
+	    int i=0;
+        while(i<100){
+        System.out.println("Hello World");
+		i=i+1;
+	}
     }
 }
